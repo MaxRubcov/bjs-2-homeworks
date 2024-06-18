2c5e5a114c25a4dfe0cdc9c107a73e3ed87e98e4
MaxRU <m_rubcov@bk.ru>
2024-06-18 22:53:28 +0300

    first homework solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let D = Math.pow(b, 2) - 4 * a * c;
+	if (D < 0) {
+		arr = [];
+	} else if (D === 0) {
+		arr[0] = -b / (2 * a);
+	} else if (D > 0) {
+		arr[0] = (-b + Math.sqrt(D)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(D)) / (2 * a);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let interestRate = parseInt(percent) / 100 / 12; //процентная ставка
+	let initialFee = parseInt(contribution); //начальный взнос 
+	let totalCost = parseInt(amount); //общая стоимость
+	let creditTerm = parseInt(countMonths); // срок кредита
+	let s = totalCost - initialFee; //тело кредита
+	let pay = s * (interestRate + interestRate / (((1 + interestRate) ** creditTerm) - 1)); //ежемесячная оплата
+	let totalAmount = +(pay * creditTerm).toFixed(2);
+	console.log(totalAmount);
+
+
+	return totalAmount;
 }
